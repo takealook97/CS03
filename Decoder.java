7024f859430b55ebc1f4d0283ceda074ecdb97b8
takealook <takealook97@naver.com>
2023-01-10 16:55:14 +0900

    decode 클래스 생성

---
+++ Decoder.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+
+public class Decoder {
+    String[] decode(String[] input) {
+        switch (input[0]) {
+            case "0001", "0011", "0101", "0110", "0111", "1001" -> {
+                String[] decode = {instDecode(input[0]), regDecode(input[1]),
+                        regDecode(input[2]), regDecode(input[4])};
+                return decode;
+            }
+            case "0010", "0100", "1000", "1010" -> {
+                String[] decode = {instDecode(input[0]), regDecode(input[1]),
+                        regDecode(input[2]), valDecode(input[4])};
+                return decode;
+            }
+            case "1011" -> {
+                String[] decode = {instDecode(input[0]), regDecode(input[1]),
+                        valDecode(input[2])};
+                return decode;
+            }
+        }
+        return input;
+    }
+
+    String instDecode(String input) {
+        HashMap<String, String> instruction = new HashMap<>();
+        instruction.put("0001", "LOAD");
+        instruction.put("0010", "LOAD");
+        instruction.put("0011", "STORE");
+        instruction.put("0100", "STORE");
+        instruction.put("0101", "AND");
+        instruction.put("0110", "OR");
+        instruction.put("0111", "ADD");
+        instruction.put("1000", "ADD");
+        instruction.put("1001", "SUB");
+        instruction.put("1010", "SUB");
+        instruction.put("1011", "MOV");
+        return instruction.get(input);
+    }
+
+    String regDecode(String input) {
+        HashMap<String, String> registry = new HashMap<>();
+        registry.put("001", "R1");
+        registry.put("010", "R2");
+        registry.put("011", "R3");
+        registry.put("100", "R4");
+        registry.put("101", "R5");
+        registry.put("110", "R6");
+        registry.put("111", "R7");
+        return registry.get(input);
+    }
+
+    String valDecode(String input) {
+        return Integer.toString(Integer.parseInt(input, 2));
+    }
+
+}
--- Main.java
+++ Main.java
@@ -8,12 +8,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashMap<String, String> memory = new HashMap<>();//메모리주소 : 메모리값 해시맵
-        Method Method = new Method();
+        Decoder Decoder = new Decoder();
         while (true) {
             String[] input = br.readLine().split(" ");
-            System.out.println(Arrays.toString(Method.decode(input)));
+            System.out.println(Arrays.toString(Decoder.decode(input)));
         }
-
-
     }
 }
