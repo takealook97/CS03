1a098fec570063d1d9148688db2fba9dd1e88436
takealook <takealook97@naver.com>
2023-01-10 16:05:12 +0900

    decode 만들기

--- CPU.java
+++ CPU.java
@@ -1,54 +1,19 @@
 import java.util.HashMap;
 
 public class CPU {
-    String bitcode(String[] input) {
-        switch (input[1]) {//4bit instruction
-            case "0001"://LOAD
-
-                break;
-            case "0010"://LOAD
-
-                break;
-            case "0011"://STORE
-
-                break;
-            case "0100"://STORE
-
-                break;
-            case "0101"://AND
-
-                break;
-            case "0110"://OR
-
-                break;
-            case "0111"://ADD
-
-                break;
-            case "1000"://ADD
-
-                break;
-            case "1001"://SUB
-
-                break;
-            case "1010"://SUB
-
-                break;
-            case "1011"://MOV
-
-                break;
-
-
-        }
-    }
-    String register(String[] input) {
-        String PC;
-        HashMap<String, String> register = new HashMap<>();//regist 3bit : ? 구조
-
-        return "";
+    String instruction(String[] input) {
+        return "a";
     }
 
-    String ALU (String A, String B) {
+    String register(String input) {//레지스터에는 001과 같은 레지스터의 위치값
+        return "a";
+    }
+    String print(String[] input){
+        return "a";
+    }
 
+    String ALU(String A, String B) {
+        return "a";
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,47 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashMap<String, String> memory = new HashMap<>();//메모리주소 : 메모리값 해시맵
-        HashMap<String, String> register = new HashMap<>();
-        CPU cpu = new CPU();
+        Method Method = new Method();
         while (true) {
             String[] input = br.readLine().split(" ");
-            memory.put(input[0], cpu.bitcode(input));
-
+            System.out.println(Arrays.toString(Method.decode(input)));
         }
 
 
-//            switch (input[1]) {//메모리주소 - instruction - reg~
-//                case "LOAD":
-//                    if(input[4].charAt(0) == 'R'){
-//
-//                    }
-//                    break;
-//                case "STORE":
-//
-//                    break;
-//                case "AND":
-//
-//                    break;
-//                case "OR":
-//
-//                    break;
-//                case "ADD":
-//
-//                    break;
-//                case "SUB"://Substraction
-//
-//                    break;
-//                case "MOV"://Move data
-//                    input[2]
-//                    break;
-//                default :
-//                    System.out.println("WRONG INSTRUCTION");
-//            }
     }
 }
